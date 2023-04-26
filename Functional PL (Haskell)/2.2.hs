5bcb6f31cdcb1667ee996a97956488bad3515a7f
Valerii <83455666+fur1ouswolf@users.noreply.github.com>
2023-04-26 12:26:10 +0500

    Update 2.2.hs

--- Functional PL (Haskell)/2.2.hs
+++ Functional PL (Haskell)/2.2.hs
@@ -1,14 +1,12 @@
-joseph :: Integer -> Integer -> Integer
+f :: [String] -> String
+f list = list !! ((joseph (length list) 5) - 1)
+
+joseph :: Int -> Int -> Int
 joseph n k | n > 1 = ((joseph (n - 1) k + k - 1) `mod` n) + 1
            | otherwise = 1
 
-test :: Integer -> Integer -> String
-test n ans = if joseph n 5 == ans then "OK" else "WA"
+test :: [String] -> String -> String
+test s ans = if f s == ans then "OK" else "WA"
 
 main = do
-  print("Test 1: " ++ test 10 3)
-  print("Test 2: " ++ test 1 1)
-  print("Test 3: " ++ test 2 2)
-  print("Test 4: " ++ test 5 2)
-  print("Test 5: " ++ test 7 6)
-  print("Test 6: " ++ test 20 7)
+  print("Test 1: " ++ test ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"] "3")
