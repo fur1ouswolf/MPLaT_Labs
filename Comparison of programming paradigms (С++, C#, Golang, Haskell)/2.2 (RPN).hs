04dbae7923e5d77a67134085423ba53f8021d9f2
Valerii <83455666+fur1ouswolf@users.noreply.github.com>
2023-06-23 22:11:31 +0500

    Update 2.2 (RPN).hs

--- Comparison of programming paradigms (С++, C#, Golang, Haskell)/2.2 (RPN).hs
+++ Comparison of programming paradigms (С++, C#, Golang, Haskell)/2.2 (RPN).hs
@@ -1,3 +1,8 @@
+import Control.Monad (replicateM_)
+import Data.List (delete, (\\))
+import System.CPUTime
+import System.Process (callCommand)
+
 calc :: String -> [Integer]
 calc = foldl f [] . words
     where
@@ -7,3 +12,12 @@ calc = foldl f [] . words
      f (x:y:zs) "*"    = (y * x):zs
      f (x:y:zs) "/"    = (y `div` x):zs
      f xs       y      = read y : xs
+
+main = do
+    print(calc "100 6 *")
+    start <- getCPUTime
+    replicateM_ 100000 $ do
+        return (calc "100000000000000000 6 *")
+    end <- getCPUTime
+    let diff = (fromIntegral (end - start)) / (10 ^ 8)
+    putStrLn $ "Time taken: " ++ show diff ++ " milliseconds"
